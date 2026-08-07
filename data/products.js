007e0c1232a137aa769ad28b312924e4da4241d3
Yuixixixixi <yendra.paundra@gmail.com>
2026-08-07 09:47:49 +0700

    Sprint1-add perbaikan minor

---
+++ data/products.js
@@ -0,0 +1,12 @@
+const products = [
+  { id: 1, name: "Beras Premium 5kg", category: "sembako", price: 65000, stock: 20, unit: "karung", image: "/img/beras.svg", description: "Beras putih premium pulen untuk kebutuhan harian." },
+  { id: 2, name: "Minyak Goreng 2L", category: "sembako", price: 34000, stock: 15, unit: "pouch", image: "/img/minyak.svg", description: "Minyak goreng kemasan pouch 2 liter." },
+  { id: 3, name: "Gula Pasir 1kg", category: "sembako", price: 16000, stock: 30, unit: "pack", image: "/img/gula.svg", description: "Gula pasir kristal putih 1 kilogram." },
+  { id: 4, name: "Telur Ayam 1kg", category: "sembako", price: 29000, stock: 12, unit: "kg", image: "/img/telur.svg", description: "Telur ayam negeri segar." },
+  { id: 5, name: "Sabun Cuci Piring 800ml", category: "kebersihan", price: 18000, stock: 25, unit: "botol", image: "/img/sabun.svg", description: "Sabun cuci piring wangi lemon." },
+  { id: 6, name: "Deterjen Bubuk 900g", category: "kebersihan", price: 22000, stock: 18, unit: "pack", image: "/img/deterjen.svg", description: "Deterjen bubuk hemat." },
+  { id: 7, name: "Kopi Sachet Isi 10", category: "minuman", price: 12000, stock: 40, unit: "renceng", image: "/img/kopi.svg", description: "Kopi instan sachet praktis." },
+  { id: 8, name: "Teh Celup Isi 25", category: "minuman", price: 9500, stock: 35, unit: "box", image: "/img/teh.svg", description: "Teh celup wangi, 25 kantong." },
+];
+
+module.exports = products;
